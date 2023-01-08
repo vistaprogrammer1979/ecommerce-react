623a7e0e4b99b2ecceafc2f725999b39891736d8
unknown <vistaprogrammer@gmail.com>
2023-01-08 15:38:02 -0800

    ecommerce react

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>Copyright &copy; Basel Sami Alnjjad - Senior Web Developer</Col>
+        </Row>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
